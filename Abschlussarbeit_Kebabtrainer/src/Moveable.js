a7a2746ef1c309169738b5b32159cf5374f0a344
marcok06 <48826814+marcok06@users.noreply.github.com>
2022-02-09 18:49:49 +0100

    add moveable class

---
+++ Abschlussarbeit_Kebabtrainer/src/Moveable.js
@@ -0,0 +1,30 @@
+"use strict";
+var Kebabtrainer;
+(function (Kebabtrainer) {
+    class Moveable extends Kebabtrainer.Drawable {
+        speed = 0.02;
+        targetPosition = new Kebabtrainer.Vector(0, 0);
+        constructor(_pos) {
+            super(_pos);
+        }
+        update() {
+            super.update();
+            this.move();
+        }
+        move() {
+            // Calculate the direction to the target
+            let _dir = this.targetPosition.copy();
+            _dir.subtract(this.position);
+            _dir.scale(this.speed);
+            this.position.add(_dir);
+        }
+        setTarget(_target) {
+            this.targetPosition = _target;
+        }
+        draw() {
+            console.log("Moveable.draw()");
+        }
+    }
+    Kebabtrainer.Moveable = Moveable;
+})(Kebabtrainer || (Kebabtrainer = {}));
+//# sourceMappingURL=Moveable.js.map
---
+++ Abschlussarbeit_Kebabtrainer/src/Moveable.ts
@@ -0,0 +1,31 @@
+namespace Kebabtrainer {
+    export class Moveable extends Drawable {
+        protected speed: number = 0.02;
+        protected targetPosition: Vector = new Vector(0, 0);
+
+        constructor(_pos: Vector) {
+            super(_pos);
+        }
+
+        public update(): void {
+            super.update();
+            this.move();
+        }
+
+        public move(): void {
+            // Calculate the direction to the target
+            let _dir: Vector = this.targetPosition.copy();
+            _dir.subtract(this.position);
+            _dir.scale(this.speed);
+            this.position.add(_dir);
+        }
+
+        public setTarget(_target: Vector): void {
+            this.targetPosition = _target;
+        }
+
+        public draw(): void {
+            console.log("Moveable.draw()");
+        }
+    }
+}
