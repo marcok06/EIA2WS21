8070cc0b369166ad436dcc9dc20da03d57880924
marcok06 <48826814+marcok06@users.noreply.github.com>
2022-02-10 22:09:40 +0100

    customer wishes, position and mood

--- Abschlussarbeit_Kebabtrainer/src/Customer.js
+++ Abschlussarbeit_Kebabtrainer/src/Customer.js
@@ -5,8 +5,79 @@ var Kebabtrainer;
         ingredientWishes = [];
         timeWaited = 0;
         served = false;
+        constructor(_shop, _pos) {
+            super(_shop, _pos);
+            this.makeRandomWishes();
+        }
+        update() {
+            super.update();
+            // Don't do anything if customer is served already
+            if (this.served == true) {
+                return;
+            }
+            // Time waited
+            this.timeWaited += 1;
+            // Change mood depending on time waited
+            if (this.timeWaited < 1000) {
+                this.mood = Kebabtrainer.Mood.Happy;
+            }
+            else if (this.timeWaited < 2000) {
+                this.mood = Kebabtrainer.Mood.Neutral;
+            }
+            else if (this.timeWaited < 3000) {
+                this.mood = Kebabtrainer.Mood.Sad;
+            }
+            else {
+                this.mood = Kebabtrainer.Mood.Angry;
+            }
+            this.updatePostion();
+        }
+        updatePostion() {
+            // Set target position for customers. 
+            for (let i = 0; i < this.shop.customers.length; i++) {
+                if (this.shop.customers[i] == this) {
+                    // Multiply 100 by the index, so customers stand behind each other in a row
+                    let position = new Kebabtrainer.Vector(this.shop.customerWaitingPosition.x + (i * 100), this.shop.customerWaitingPosition.y);
+                    // Set target position
+                    this.setTarget(position);
+                    // Draw ingredient wishes for the first customer in row
+                    if (i == 0) {
+                        this.drawWishes();
+                    }
+                }
+            }
+        }
+        makeRandomWishes() {
+            // Create random number of wishes
+            let numberOfWishes = Math.floor(Math.random() * 3) + 1;
+            // Choose random ingredient for each wish
+            for (let i = 0; i < numberOfWishes; i++) {
+                let randomIngredient = Math.floor(Math.random() * 5);
+                this.ingredientWishes.push(randomIngredient);
+            }
+        }
+        becomServed(_ingredients) {
+            // Set customer as served
+            this.served = true;
+            // Remove customer from the shop array and add it to the served array
+            this.shop.customers.splice(this.shop.customers.indexOf(this), 1);
+            this.shop.customersServed.push(this);
+            // Check if we have all the ingredients
+            let isOrderRight = true;
+            if (_ingredients.length != this.ingredientWishes.length) {
+                isOrderRight = false;
+            }
+            // Update customers mood
+            if (isOrderRight) {
+                this.mood = Kebabtrainer.Mood.Happy;
+            }
+            else {
+                this.mood = Kebabtrainer.Mood.Angry;
+            }
+            this.targetPosition = new Kebabtrainer.Vector(this.shop.customerWaitingPosition.x, this.shop.customerWaitingPosition.y + 1000);
+        }
         drawWishes() {
-            // Draw ingredients
+            // Draw customers wishes
             for (let i = 0; i < this.ingredientWishes.length; i++) {
                 switch (this.ingredientWishes[i]) {
                     case Kebabtrainer.Ingredient.Salad:
--- Abschlussarbeit_Kebabtrainer/src/Customer.ts
+++ Abschlussarbeit_Kebabtrainer/src/Customer.ts
@@ -5,9 +5,93 @@ namespace Kebabtrainer {
         public timeWaited: number = 0;
         public served: boolean = false;
 
+        constructor(_shop: Shop, _pos: Vector) {
+            super(_shop, _pos);
+
+            this.makeRandomWishes();
+        }
+
+        public update(): void {
+            super.update();
+
+            // Don't do anything if customer is served already
+            if (this.served == true) {
+                return;
+            }
+
+            // Time waited
+            this.timeWaited += 1;
+
+            // Change mood depending on time waited
+            if (this.timeWaited < 1000) {
+                this.mood = Mood.Happy;
+            } else if (this.timeWaited < 2000) {
+                this.mood = Mood.Neutral;
+            } else if (this.timeWaited < 3000) {
+                this.mood = Mood.Sad;
+            } else {
+                this.mood = Mood.Angry;
+            }
+
+            this.updatePostion();
+        }
+
+        public updatePostion(): void {
+            // Set target position for customers. 
+            for (let i: number = 0; i < this.shop.customers.length; i++) {
+                if (this.shop.customers[i] == this) {
+
+                    // Multiply 100 by the index, so customers stand behind each other in a row
+                    let position: Vector = new Vector(this.shop.customerWaitingPosition.x + (i * 100), this.shop.customerWaitingPosition.y);
+
+                    // Set target position
+                    this.setTarget(position);
+
+                    // Draw ingredient wishes for the first customer in row
+                    if (i == 0) {
+                        this.drawWishes();
+                    }
+                }
+            }
+        }
+
+        public makeRandomWishes() {
+            // Create random number of wishes
+            let numberOfWishes: number = Math.floor(Math.random() * 3) + 1;
+
+            // Choose random ingredient for each wish
+            for (let i: number = 0; i < numberOfWishes; i++) {
+                let randomIngredient: Ingredient = Math.floor(Math.random() * 5);
+                this.ingredientWishes.push(randomIngredient);
+            }
+        }
+
+        public becomServed(_ingredients: Ingredient[]): void {
+            // Set customer as served
+            this.served = true;
+
+            // Remove customer from the shop array and add it to the served array
+            this.shop.customers.splice(this.shop.customers.indexOf(this), 1);
+            this.shop.customersServed.push(this);
+
+            // Check if we have all the ingredients
+            let isOrderRight: boolean = true;
+            if (_ingredients.length != this.ingredientWishes.length) {
+                isOrderRight = false;
+            }
+
+            // Update customers mood
+            if (isOrderRight) {
+                this.mood = Mood.Happy;
+            } else {
+                this.mood = Mood.Angry;
+            }
+
+            this.targetPosition = new Vector(this.shop.customerWaitingPosition.x, this.shop.customerWaitingPosition.y + 1000);
+        }
 
         public drawWishes(): void {
-            // Draw ingredients
+            // Draw customers wishes
             for (let i: number = 0; i < this.ingredientWishes.length; i++) {
                 switch (this.ingredientWishes[i]) {
                     case Ingredient.Salad:
